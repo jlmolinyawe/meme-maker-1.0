e01ffedf9dd19c4671c78ca4827aedd2fd5b0e34
MOLINYAWE, JUSTIN L <jm043a@us.att.com>
2019-08-26 11:19:13 -0700

    First commit

--- MemeMe1/ViewController.swift
+++ MemeMe1/ViewController.swift
@@ -8,13 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.blue,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  Float(3)
+    ]
+    
+    let topDefaultValue = "TOP"
+    let bottomDefaultValue = "BOTTOM"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+ 
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        topTextField.text = topDefaultValue
+        bottomTextField.text = bottomDefaultValue
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
     }
 
+    @IBAction func selectImage(_ sender: Any) {
+        let imageController = UIImagePickerController()
+        imageController.delegate = self
+        imageController.sourceType = .photoLibrary
+        present(imageController, animated: true, completion: nil)
+    }
+    
+    @IBAction func takeImageFromCamera(_ sender: Any) {
+        let imageController = UIImagePickerController()
+        imageController.delegate = self
+        imageController.sourceType = .camera
+        present(imageController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            mainImageView.image = image
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
 
 }
 
